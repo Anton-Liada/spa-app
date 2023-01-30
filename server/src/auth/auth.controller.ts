7a77899cf2e4f2a92b31b77de29ff5cdcf31b162
Anton Liada <liadaantom@gmail.com>
2023-01-30 17:57:42 +0200

    Server was implemented

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/signin')
+  async login(@Body() userDto: LoginUserDto) {
+    return await this.authService.login(userDto);
+  }
+
+  @Post('/signup')
+  async registration(@Body() userDto: CreateUserDto) {
+    return await this.authService.registration(userDto);
+  }
+
+  @Get('/logout')
+  logout(@Request() req): any {
+    req.session.destroy();
+    return { message: 'The user session has ended' };
+  }
+}
