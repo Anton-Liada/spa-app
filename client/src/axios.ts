df26187cb774a325dd9b1753e4895a155d4dbe9c
Anton Liada <liadaantom@gmail.com>
2023-08-06 16:42:58 +0300

    BASE_URL was changed

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000/';
+export const BASE_URL = 'https://nest-server-omcz.onrender.com';
 
 const instance = axios.create({
   baseURL: BASE_URL,
